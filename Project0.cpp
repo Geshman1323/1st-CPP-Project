364812020c50cf9fc102138835e460c32b649f5f
Geshman Shakeel <113760897+Geshman1323@users.noreply.github.com>
2022-11-23 07:45:17 -0800

    Add files via upload

---
+++ Project0.cpp
@@ -0,0 +1,33 @@
+// my first Program
+// This will Include header file
+#include <iostream>
+
+using namespace std;
+
+// this is the main is function needed to run this program
+int main()
+{
+    // DataTypes ----> LONG, INT , CHAR , STRING , FLOAT , DOUBLE , bool
+    
+    int num1 ,num2;
+    string name;
+
+    string Dia1 = "Enter Your name ";
+
+    cout<<Dia1<<"\n";
+    cin>>name;
+
+    string Dia2 = "Enter 1st number for sum";
+
+    cout<<name<<", "<<Dia2<<"\n";
+    cin>>num1;
+
+    string Dia3 = "Enter 2nd number for sum";
+
+    cout<<Dia3<<"\n";
+    cin>>num2;
+
+    cout<<num1 + num2;
+
+    return 0; 
+}
