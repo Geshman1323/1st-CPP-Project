46e6d746d641a3dbc3e74fde4fd8d292285f4795
Geshman Shakeel <113760897+Geshman1323@users.noreply.github.com>
2022-12-20 15:10:06 +0500

    Update Project0.cpp

--- Project0.cpp
+++ Project0.cpp
@@ -8,11 +8,10 @@ int main() /* All code inside this function would be excuted first and given pri
     // Basics Intro of C++ that is as follow
     // it is low level , fast , OOP-programming language developed by  Bjarne Stroustrup in 1979 and he named it in 1983 as C++
     // it was made first as an extension of C to increase its capabilties
-    // this language is use to make game engine like unreal use this language moreover it is use to make very very fast programs
+    // game engine like unreal use this language moreover it is use to make very fast programs
     // DataTypes ----> LONG , INT , CHAR , STRING , FLOAT , DOUBLE , bool , Long Double .etc.
-    // print Method and Geting Input method what ever you say
     // Operaters Including Arithmatic , Logical , assignment , comparison .etc.
-    // Basically most of the things i've learnt is similar to previous languages I have learnt in the past
+    // Basically most of the things i've learnt is similar to languages I have learnt in the past
     
     int num1 ,num2;
     string name;
